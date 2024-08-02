db53e48425cdd039df0b6891d9f20265135975b9
KoyomiHidaka <abbishev@mail.ru>
2024-08-02 18:19:30 +0500

    DATASET PREPARING

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,5 +1,6 @@
 import nltk
-nltk.download('punkt')
+import numpy as np
+#nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 
 stemmer = PorterStemmer()
@@ -12,15 +13,18 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_sentence, all_words):
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
 
-a = "Тест русский язык"
+    bag = np.zeros(len(all_words), dtype=np.float32)
 
-print(a)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1.0
+    return bag
 
-a = tokenize(a)
 
-print(a)
+
+
 
 
 
